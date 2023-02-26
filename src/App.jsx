93155eee20e22979817019bc77de602904467874
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-26 21:48:01 +1100

    installed react routing & included a test 'product' route (also changed UI font)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>fruGo</h1>
+      <Routes>
+        <Route path='/products' element={<h1>PRODUCTS</h1>} />
+      </Routes>
     </div>
   );
 }
