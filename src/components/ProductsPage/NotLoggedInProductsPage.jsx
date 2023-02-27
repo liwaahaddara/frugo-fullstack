5b4e6b43c21ef28fe9919cf227e595c9ef3c0e78
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-28 01:24:01 +1100

    added ProductList component to the Product route

--- src/components/ProductsPage/NotLoggedInProductsPage.jsx
+++ src/components/ProductsPage/NotLoggedInProductsPage.jsx
@@ -1,10 +1,13 @@
+// import UI components
 import NotLoggedInNavBar from "../NavBar/NotLoggedInNavBar"
+import ProductList from "./ProductList"
 
 export default function NotLoggedInProductsPage() {
     return (
         <>
             <NotLoggedInNavBar />
             <h1>PRODUCTS</h1>
+            <ProductList />
         </>
     )
 }
