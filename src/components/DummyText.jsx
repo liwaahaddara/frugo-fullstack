d4437815b92e7135c72819a0a07413bfca9ea818
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-27 15:07:55 +1100

    edited export statement for DummyText (also deleted old Navbar and HomePage files)

--- src/components/DummyText.jsx
+++ src/components/DummyText.jsx
@@ -1,10 +1,10 @@
-export default function DummyText() {
+function DummyText() {
     return (
         <div className="dummy-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, qui modi ab iusto magni nemo velit quis a earum, odit mollitia soluta similique vero laboriosam libero. Et sint iste repellendus consequatur nam sunt, cumque facere, enim libero natus quasi eum possimus laboriosam inventore tenetur sequi voluptas doloremque illo ab magnam consectetur aperiam voluptate odio.</div>
     )
 }
 
-export function DummyTextGroup() {
+export default function DummyTextGroup() {
     return (
         <div className="dummy-text-group">
             <DummyText />
