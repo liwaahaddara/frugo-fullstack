2867c1d4fd848afc06fe63f84d7dc632fc4cb2af
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-27 15:06:38 +1100

    refactored NavBar (began creating different versions suitable for each page); created a new one for the 'NotFoundPage'

---
+++ src/components/NavBar/NotFoundPageNavBar.jsx
@@ -0,0 +1,46 @@
+import { AppBar, Box, Container, Toolbar, Button } from "@mui/material"
+import { NavLink } from "react-router-dom"
+
+
+export default function NotFoundPageNavBar() {
+    const navBarItems = [
+        {
+            title: "Home",
+            linkTo: "/",
+        },
+    ]
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: "flex" }}>
+                        {navBarItems.map((item) => {
+                            return (
+                                <NavLink
+                                    key={item.title}
+                                    style={{
+                                        textDecoration: "none",
+                                    }
+                                    }
+                                    to={item.linkTo}
+                                >
+                                    <Button
+                                        sx={{
+                                            my: 1,
+                                            color: "white",
+                                            display: "block",
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Button>
+                                </NavLink>
+                            )
+                        })}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
---
+++ src/components/NavBar/NotLoggedInNavBar.jsx
@@ -0,0 +1,58 @@
+import { AppBar, Box, Container, Toolbar, Button } from "@mui/material"
+import { NavLink } from "react-router-dom"
+
+
+export default function NotLoggedInNavBar() {
+    const navBarItems = [
+        {
+            title: "Home",
+            linkTo: "/",
+        },
+        {
+            title: "Products",
+            linkTo: "products",
+        },
+        {
+            title: "Login",
+            linkTo: "login",
+        },
+        {
+            title: "Register",
+            linkTo: "register",
+        },
+    ]
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: "flex" }}>
+                        {navBarItems.map((item) => {
+                            return (
+                                <NavLink
+                                    key={item.title}
+                                    style={{
+                                        textDecoration: "none",
+                                    }
+                                    }
+                                    to={item.linkTo}
+                                >
+                                    <Button
+                                        sx={{
+                                            my: 1,
+                                            color: "white",
+                                            display: "block",
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Button>
+                                </NavLink>
+                            )
+                        })}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
