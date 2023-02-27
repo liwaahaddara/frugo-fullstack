11a195d7035ba009e388104bef3b8921776f98ae
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-27 12:31:23 +1100

    created 'DummyText' for the HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,8 @@
 import { Outlet, NavLink } from "react-router-dom"
+
+// UI Components for HomePage
 import NavBar from "./styled/NavBar"
+import { DummyTextGroup } from "./DummyText"
 
 
 
@@ -16,6 +19,9 @@ export default function HomePage() {
                 <h1 className="fruGo-title">fruGo</h1>
             </NavLink>
             <NavBar />
+
+            <h2>YOUR HOME ESSENTIALS, DELIVERED STRAIGHT TO YOUR DOOR!</h2>
+            <DummyTextGroup />
             <Outlet />
         </>
     )
