ae3964a5ea4acf028cca3c67540ee73f38d13830
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-27 15:10:23 +1100

    cleaned up Routes on 'App.jsx' to show the refactored, logged-out components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 // Below are import commands for the UI components
-import HomePage from './components/HomePage';
+import NotLoggedInHomePage from './components/HomePage/NotLoggedInHomePage';
+import NotLoggedInProductsPage from './components/ProductsPage/NotLoggedInProductsPage';
 import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
+      <NavLink
+        key="fruGo-title"
+        style={{
+          textDecoration: "none"
+        }}
+        to="/"
+      >
+        <h1 className="fruGo-title">fruGo</h1>
+      </NavLink>
       <Routes>
-        <Route exact path='/' element={<HomePage />}>
-          <Route exact path='products' element={<h1>PRODUCTS</h1>} />
-          <Route exact path="*" element={<NotFound />} />
-        </Route>
+        <Route exact path='/' element={<NotLoggedInHomePage />} />
+
+        <Route exact path='products' element={<NotLoggedInProductsPage />} />
+
+        <Route exact path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
