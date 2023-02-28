9741d53bbba1aeaa684d7971997175650a7cf6b6
Liwaa Haddara <12786@coderacademy.edu.au>
2023-02-28 12:08:45 +1100

    created a Customer NavBar

---
+++ src/components/NavBar/CustomerNavbar.jsx
@@ -0,0 +1,58 @@
+import { AppBar, Box, Container, Toolbar, Button } from "@mui/material"
+import { NavLink } from "react-router-dom"
+
+
+export default function CustomerNavBar() {
+    const navBarItems = [
+        {
+            title: "Home",
+            linkTo: "/",
+        },
+        {
+            title: "Products",
+            linkTo: "/customer/products",
+        },
+        {
+            title: "Cart",
+            linkTo: "/customer/cart",
+        },
+        {
+            title: "Orders",
+            linkTo: "/customer/orders",
+        },
+    ]
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: "flex" }}>
+                        {navBarItems.map((item) => {
+                            return (
+                                <NavLink
+                                    key={item.title}
+                                    style={{
+                                        textDecoration: "none",
+                                    }
+                                    }
+                                    to={item.linkTo}
+                                >
+                                    <Button
+                                        sx={{
+                                            my: 1,
+                                            color: "white",
+                                            display: "block",
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Button>
+                                </NavLink>
+                            )
+                        })}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
