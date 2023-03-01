bafaf076a796900e2bf16dfd341f9f9c803f7295
Tāne K <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 17:29:11 +1100

    Revert "Fetch data for `<ProductList>`"

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,42 @@
-import "./App.css";
-import { Route, Routes, NavLink } from "react-router-dom";
+import './App.css';
+import { Route, Routes, NavLink } from 'react-router-dom';
 
 // Below are import commands for the UI components
-import NotLoggedInHomePage from "./components/HomePage/NotLoggedInHomePage";
-import NotLoggedInProductsPage from "./components/ProductsPage/NotLoggedInProductsPage";
-import Login from "./components/Login";
-import NotFound from "./components/NotFound";
-import Register from "./components/Register";
-import CustomerHomePage from "./components/CustomerPages/CustomerHomepage";
-import CustomerProductsPage from "./components/CustomerPages/CustomerProductsPage";
-import CustomerCartPage from "./components/CustomerPages/CustomerCartPage";
-import CustomerOrderConfirmation from "./components/CustomerPages/CustomerOrderConfirmation";
-import { useState } from "react";
+import NotLoggedInHomePage from './components/HomePage/NotLoggedInHomePage';
+import NotLoggedInProductsPage from './components/ProductsPage/NotLoggedInProductsPage';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+import Register from './components/Register';
+import CustomerHomePage from './components/CustomerPages/CustomerHomePage';
+import CustomerProductsPage from './components/CustomerPages/CustomerProductsPage';
+import CustomerCartPage from './components/CustomerPages/CustomerCartPage';
+import CustomerOrderConfirmation from './components/CustomerPages/CustomerOrderConfirmation';
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState("");
-  const [token, setToken] = useState("");
   return (
     <div className="App">
       <NavLink
         key="fruGo-title"
         style={{
-          textDecoration: "none",
+          textDecoration: "none"
         }}
         to="/"
       >
         <h1 className="fruGo-title">fruGo</h1>
       </NavLink>
       <Routes>
-        <Route exact path="/" element={<NotLoggedInHomePage />} />
+        <Route exact path='/' element={<NotLoggedInHomePage />} />
 
-        <Route exact path="products" element={<NotLoggedInProductsPage />} />
+        <Route exact path='products' element={<NotLoggedInProductsPage />} />
 
-        <Route
-          exact
-          path="login"
-          element={
-            <Login
-              loggedInUser={loggedInUser}
-              setLoggedInUser={setLoggedInUser}
-              setToken={setToken}
-              token={token}
-            />
-          }
-        />
+        <Route exact path='login' element={<Login />} />
 
-        <Route exact path="register" element={<Register />} />
+        <Route exact path='register' element={<Register />} />
 
-        <Route exact path="customer" element={<CustomerHomePage />} />
-        <Route
-          exact
-          path="customer/products"
-          element={<CustomerProductsPage />}
-        />
-        <Route exact path="customer/cart" element={<CustomerCartPage />} />
-        <Route
-          exact
-          path="customer/order-confirmation"
-          element={<CustomerOrderConfirmation />}
-        />
+        <Route exact path='customer' element={<CustomerHomePage />} />
+        <Route exact path='customer/products' element={<CustomerProductsPage />} />
+        <Route exact path='customer/cart' element={<CustomerCartPage />} />
+        <Route exact path='customer/order-confirmation' element={<CustomerOrderConfirmation />} />
 
         <Route exact path="*" element={<NotFound />} />
       </Routes>
--- src/components/ProductsPage/NotLoggedInProductList.jsx
+++ src/components/ProductsPage/NotLoggedInProductList.jsx
@@ -1,41 +1,78 @@
 // import hardcoded product images (fruits/veg)
-// import Apples from "../images/apples.jpg";
-// import Cherries from "../images/cherries.jpg";
-// import Carrots from "../images/carrots.jpg";
-// import Potatoes from "../images/potatoes.jpg";
-// import Oranges from "../images/oranges.jpg";
-// import Tomatoes from "../images/tomatoes.jpg";
+import Apples from "../images/apples.jpg"
+import Cherries from "../images/cherries.jpg"
+import Carrots from "../images/carrots.jpg"
+import Potatoes from "../images/potatoes.jpg"
+import Oranges from "../images/oranges.jpg"
+import Tomatoes from "../images/tomatoes.jpg"
 
-import styled from "styled-components";
+import styled from "styled-components"
 
 //importing default exports - name can be different
-import Product from "./Product";
+import Product from "./Product"
 
 // import styled UI components
-import GridBox from "../styled/GridBox";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import GridBox from "../styled/GridBox"
 
 export const CustomGrid = styled(GridBox)`
-  padding: 30px;
-`;
+    padding: 30px;
+`
+
+
 
 export default function NotLoggedInProductList(props) {
-  const { products } = props;
-  return (
-    <div id="products">
-      <CustomGrid>
-        {products &&
-          products.map((product) => {
-            return (
-              <Product
-                key={product._id}
-                productInfo={product}
-                setProduct={props.setProduct}
-              />
-            );
-          })}
-      </CustomGrid>
-    </div>
-  );
+    const products = [
+        {
+            id: 1,
+            name: "Apple",
+            type: "Fruit",
+            image: Apples,
+        },
+        {
+            id: 2,
+            name: "Cherry",
+            type: "Fruit",
+            image: Cherries,
+        },
+        {
+            id: 3,
+            name: "Carrot",
+            type: "Vegetable",
+            image: Carrots,
+        },
+        {
+            id: 4,
+            name: "Potatoe",
+            type: "Vegetable",
+            image: Potatoes,
+        },
+        {
+            id: 5,
+            name: "Orange",
+            type: "Fruit",
+            image: Oranges,
+        },
+        {
+            id: 6,
+            name: "Tomato",
+            type: "Vegetable",
+            image: Tomatoes,
+        },
+
+    ]
+
+
+    return (
+        <div id="products">
+            <CustomGrid>
+                {products.map((product) => {
+                    return <Product
+                        key={product.id}
+                        productInfo={product}
+                        setProduct={props.setProduct} />
+                })}
+            </CustomGrid>
+        </div>
+    )
 }
+
