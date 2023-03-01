2569ad5fe0c3d2ab5084e846047a3889da8fb48e
Liwaa Haddara <12786@coderacademy.edu.au>
2023-03-01 11:53:21 +1100

    created components for a CustomerCartPage

---
+++ src/components/CustomerPages/CustomerCartItem.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+const CustomerCartItem = (props) => {
+
+    const [noOfItems, setNoOfitems] = useState(0)
+
+    const item = props.item
+
+    const handleDecrease = () => {
+        setNoOfitems((prevState) => {
+            return prevState - 1
+        })
+    }
+
+    const handleIncrease = () => {
+        setNoOfitems((prevState) => {
+            return prevState + 1
+        })
+    }
+
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }}>
+            <img
+                style={{
+                    height: 200,
+                }}
+                src={item.image}
+                alt="Bag"
+            ></img>
+            <div
+                style={{
+                    fontSize: 25,
+                    fontWeight: 700,
+                    marginBottom: 10,
+                }}
+            >
+                {item.title}
+            </div>
+            <div>{item.description}</div>
+            <div
+                style={{
+                    color: "red",
+                    marginTop: 10,
+                }}
+            >
+                ${item.price}
+            </div>
+            <div>Stock: {item.stock}</div>
+            <div style={{
+                marginTop: 5
+            }}>
+                <button disabled={noOfItems === 0} onClick={handleDecrease}>-</button>
+                <span style={{
+                    margin: '0px 5px'
+                }}>{noOfItems}</span>
+                <button disabled={noOfItems === item.stock} onClick={handleIncrease}>+</button>
+            </div>
+            <div>Sub-Total Price: ${(noOfItems * item.price).toFixed(2)}</div>
+        </div>
+    )
+}
+
+export default CustomerCartItem
---
+++ src/components/CustomerPages/CustomerCartPage.jsx
@@ -0,0 +1,116 @@
+// import hardcoded product images (fruits/veg)
+import Apples from "../images/apples.jpg"
+import Cherries from "../images/cherries.jpg"
+import Carrots from "../images/carrots.jpg"
+import Potatoes from "../images/potatoes.jpg"
+import Oranges from "../images/oranges.jpg"
+import Tomatoes from "../images/tomatoes.jpg"
+
+import Title from "../styled/Title"
+import GridBox from "../styled/GridBox"
+import CustomerCartItem from "./CustomerCartItem"
+
+function CustomerCartPage() {
+    const products = [
+        {
+            id: 1,
+            name: "Apple",
+            price: 4.78,
+            type: "Fruit",
+            image: Apples,
+            stock: 10
+        },
+        {
+            id: 2,
+            name: "Cherry",
+            price: 4.78,
+            type: "Fruit",
+            image: Cherries,
+            stock: 10
+        },
+        {
+            id: 3,
+            name: "Carrot",
+            price: 4.78,
+            type: "Vegetable",
+            image: Carrots,
+            stock: 10
+        },
+        {
+            id: 4,
+            name: "Potatoe",
+            price: 4.78,
+            type: "Vegetable",
+            image: Potatoes,
+            stock: 10
+        },
+        {
+            id: 5,
+            name: "Orange",
+            price: 4.78,
+            type: "Fruit",
+            image: Oranges,
+            stock: 10
+        },
+        {
+            id: 6,
+            name: "Tomato",
+            price: 4.78,
+            type: "Vegetable",
+            image: Tomatoes,
+            stock: 10
+        },
+
+    ]
+
+    return (
+        <div id="cart">
+            <Title>Cart</Title>
+            <GridBox>
+                {products.map((item) => {
+                    return <CustomerCartItem key={item.id} item={item} />
+                })}
+            </GridBox>
+            <Title>TOTAL: $(total amount)</Title>
+            <div className="cart-bottom-buttons">
+                <span><button className="cart-bottom-button">Send Order to Merchant</button></span>
+                <span><button className="cart-bottom-button">Clear Cart</button></span>
+            </div>
+        </div>
+    )
+}
+
+
+export default CustomerCartPage
+
+
+
+
+
+
+// import { useLoaderData } from "react-router-dom"
+// import axios from "axios"
+// export function loader() {
+    //     return axios
+    //         .get("/carts/cart")
+    //         .then((res) => res.data)
+//         .then((json) => {
+//             const products = json.products
+//             let newItemsPromise = []
+//             const getProductPromise = (productId) => {
+//                 return axios
+//                     .get(`/products/${productId}`)
+//                     .then((res) => res.data)
+//                     .then((json) => {
+//                         return {
+//                             ...json,
+//                             stock: 5,
+//                         }
+//                     })
+//             }
+//             products.forEach((product) => {
+//                 newItemsPromise.push(getProductPromise(product.productId))
+//             })
+//             return Promise.all(newItemsPromise)
+//         })
+// }
