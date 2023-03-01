7d80802f88bd0c65bbc699483667656cfcd751a3
Liwaa Haddara <12786@coderacademy.edu.au>
2023-03-01 23:54:24 +1100

    created a MERCHANT navbar

---
+++ src/components/NavBar/MerchantNavBar.jsx
@@ -0,0 +1,62 @@
+import { AppBar, Box, Container, Toolbar, Button } from "@mui/material"
+import { NavLink } from "react-router-dom"
+
+
+export default function MerchantNavBar() {
+    const navBarItems = [
+        {
+            title: "Home",
+            linkTo: "/merchant",
+        },
+        {
+            title: "Products",
+            linkTo: "/merchant/products",
+        },
+        {
+            title: "Orders",
+            linkTo: "/merchant/orders",
+        },
+        {
+            title: "Logout",
+            linkTo: "/",
+        },
+        {
+            title: "View Profile",
+            linkTo: "/merchant/profile",
+        },
+    ]
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: "flex" }}>
+                        {navBarItems.map((item) => {
+                            return (
+                                <NavLink
+                                    key={item.title}
+                                    style={{
+                                        textDecoration: "none",
+                                    }
+                                    }
+                                    to={item.linkTo}
+                                >
+                                    <Button
+                                        sx={{
+                                            my: 1,
+                                            color: "white",
+                                            display: "block",
+                                        }}
+                                    >
+                                        {item.title}
+                                    </Button>
+                                </NavLink>
+                            )
+                        })}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+}
