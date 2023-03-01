b8c826866464e6161edfe63d2876882c1415e331
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2023-03-01 17:26:54 +1100

    Add `price` to `<Product>`

--- src/components/ProductsPage/Product.jsx
+++ src/components/ProductsPage/Product.jsx
@@ -1,33 +1,35 @@
 // import UI components
-import Wrapper from "../styled/Wrapper"
+import Wrapper from "../styled/Wrapper";
 
 export default function Product(props) {
+  const product = props.productInfo;
 
-    const product = props.productInfo
-
-
-    return (
-        <Wrapper>
-            <img
-                style={{
-                    height: 200,
-                }}
-                src={product.image}
-                alt={product.name}
-            ></img>
-            <div
-                style={{
-                    fontFamily: "Verdana, sans-serif",
-                    fontSize: 25,
-                    fontWeight: 700,
-                    marginBottom: 10,
-                }}
-            >
-                {product.name}
-            </div>
-            <div style={{
-                fontFamily: "Verdana, sans-serif"
-            }}>{product.type}</div>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <img
+        style={{
+          height: 200,
+        }}
+        src={product.img}
+        alt={product.name}
+      ></img>
+      <div
+        style={{
+          fontFamily: "Verdana, sans-serif",
+          fontSize: 25,
+          fontWeight: 700,
+          marginBottom: 10,
+        }}
+      >
+        {product.name}
+      </div>
+      <div
+        style={{
+          fontFamily: "Verdana, sans-serif",
+        }}
+      >
+        ${product.price}
+      </div>
+    </Wrapper>
+  );
 }
