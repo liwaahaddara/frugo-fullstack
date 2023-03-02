5d38a271bb8f70180624cb656fe25f0e905c225c
Liwaa Haddara <12786@coderacademy.edu.au>
2023-03-02 12:05:59 +1100

    created MerchantProductsPage component

---
+++ src/components/MerchantPages/MerchantProductsPage.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom"
+
+// import styled components
+import Title from "../styled/Title"
+import Button from "../styled/Button"
+
+// import UI components
+import MerchantProductList from "./MerchantProductList"
+import MerchantNavBar from "../NavBar/MerchantNavBar"
+
+export default function MerchantProductsPage() {
+    const navigate = useNavigate()
+    return (
+        <>
+            <MerchantNavBar />
+            <div className="user-info">
+                <span>Merchant/City: (Merchant City)</span><span> Welcome (Merchant)!</span>
+            </div>
+            <Title>YOUR PRODUCTS</Title>
+            <Button
+                onClick={() => { navigate(`/merchant/add-product`) }}
+            >ADD PRODUCT</Button>
+            <MerchantProductList />
+        </>
+    )
+}
