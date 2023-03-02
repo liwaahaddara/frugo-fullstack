3dcca6c793874955bfacc9df77d589b897998a46
Liwaa Haddara <12786@coderacademy.edu.au>
2023-03-02 16:12:32 +1100

    FOR DEV ONLY (won't be in final product): added links to all types of users to the homepage

--- src/components/HomePage/NotLoggedInHomePage.jsx
+++ src/components/HomePage/NotLoggedInHomePage.jsx
@@ -1,4 +1,8 @@
 import { Outlet } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
+// import styled UI components
+import Button from "../styled/Button"
 
 // UI Components for HomePage
 import NotLoggedInNavBar from "../NavBar/NotLoggedInNavBar"
@@ -7,9 +11,18 @@ import DummyTextGroup from "../DummyText"
 
 
 export default function NotLoggedInHomePage() {
+    const navigate = useNavigate()
     return (
         <>
             <NotLoggedInNavBar />
+            <section className="dev-buttons">
+                <Button onClick={() => { navigate(`/customer`) }}
+                >Customer</Button>
+                <Button onClick={() => { navigate(`/merchant`) }}
+                >Merchant</Button>
+                <Button onClick={() => { navigate(`/admin`) }}
+                >Admin</Button>
+            </section>
             <h2 style={{ color: "dodgerblue", backgroundColor: "black" }}
             >YOUR HOME ESSENTIALS, DELIVERED STRAIGHT TO YOUR DOOR!</h2>
             <DummyTextGroup />
